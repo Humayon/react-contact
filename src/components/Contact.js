0681ceb188789aa9a72c4b0d27033ef624179dfa
Humayon K <humayonkabir007@gmail.com>
2019-09-24 22:07:26 +0600

    contact toggle

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 class Contact extends Component {
+  state = {
+    showContact: false
+  };
+
   deleteContactHandler = id => () => {
     this.props.deleteContact(id);
   };
 
+  showContactHandler = () => {
+    this.setState({
+      showContact: !this.state.showContact
+    });
+  };
   render() {
     let { id, firstName, lastName, email, profession } = this.props.contact;
     return (
@@ -14,16 +23,24 @@ class Contact extends Component {
             <div className="card-content z-depth-3">
               <h3 className="card-title">
                 {firstName} {lastName}
-                <i className="material-icons right">arrow_drop_down</i>
+                <a href="#!" onClick={this.showContactHandler}>
+                  <i className="material-icons right">
+                    {this.state.showContact
+                      ? 'arrow_drop_up'
+                      : 'arrow_drop_down'}
+                  </i>
+                </a>
                 <a href="#!" onClick={this.deleteContactHandler(id)}>
                   <i className="material-icons right">delete</i>
                 </a>
                 <i className="material-icons right">edit</i>
               </h3>
-              <ul>
-                <li>{email}</li>
-                <li>{profession}</li>
-              </ul>
+              {this.state.showContact && (
+                <ul>
+                  <li>{email}</li>
+                  <li>{profession}</li>
+                </ul>
+              )}
             </div>
           </div>
         </div>
